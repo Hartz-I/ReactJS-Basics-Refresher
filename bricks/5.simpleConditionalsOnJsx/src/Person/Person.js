5bcedb2925d4e6e7406cda66966f6555805c1671
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2021-07-21 01:23:30 +0600

    added note on jsx

---
+++ bricks/5.simpleConditionalsOnJsx/src/Person/Person.js
@@ -0,0 +1,23 @@
+//name starts with capital of components
+//function name lower case
+
+//this is an stateless, dumb component. use as many of these you can
+
+import React from "react";
+import "./Person.css";
+
+//in class based components we use this.props
+const person = (props) => {
+  return (
+    <div className="Person">
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
+};
+//with curly bracket we can do simple function within
+//props.children for inbetween writtings
+export default person;
