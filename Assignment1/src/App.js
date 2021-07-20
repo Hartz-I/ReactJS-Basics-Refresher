5fc1de1a1e3384a1ca940b7acd41cdfce3ed6a94
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2021-07-20 19:37:26 +0600

    Started the assignment

---
+++ Assignment1/src/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person"; //name starts with uppercase
+
+class App extends Component {
+  //this state defining only works on class bases components
+
+  state = {
+    //it's a property of the class App
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephnie", age: 26 },
+    ],
+  };
+
+  //a method of tha class can be called on an event. hence event handler for events such as onClick onCopy etc
+  switchNameHandler = (newName) => {
+    //a way could be binding!
+    //console.log('Was clicked!')
+    //this.state.persons[0].name = 'Maximilian'; //just this won't work. need useState
+    this.setState({
+      persons: [
+        //simply merge. other states will be untouched
+        { name: newName, age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephnie", age: 27 },
+      ],
+    });
+  };
+
+  nameChangedHandler = (event) => {
+    //changing the name dynamically
+    this.setState({
+      persons: [
+        //simply merge. other states will be untouched
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 29 }, //targets the input section and then takes the value
+        { name: "Stephnie", age: 27 },
+      ],
+    });
+  };
+
+  render() {
+    //renders html
+
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    };
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react app!</h1>
+        <p>This is really working!!</p>
+        <button
+          style={style} //other way of doing style. variable above
+          onClick={
+            () =>
+              this.switchNameHandler(
+                "Maximilian!!"
+              ) /** an alternative to bind */
+          }
+        >
+          Switch Name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={
+            this.switchNameHandler.bind(this, "Max!!")
+            /**so we can pass methods to dumb components. Use bind instead of function call */
+          }
+          changed={this.nameChangedHandler}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div> //everything must be in one div
+    );
+
+    //the code gets compiled to this
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
+  }
+}
+
+export default App;
